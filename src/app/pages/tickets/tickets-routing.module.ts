1b431a2eb877b1b932d73a0ef5e252f60c4daea3
nbsyanh1998 <nbsyanh1998@gmail.com>
2021-01-02 22:44:45 +0700

    ád

--- src/app/pages/tickets/tickets-routing.module.ts
+++ src/app/pages/tickets/tickets-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { TicketsComponent } from "./tickets.component";
-import { ChooseCoachComponent } from "./choose-coach/choose-coach.component";
-import { PaymentComponent } from "./payment/payment.component";
 import { VeDaDatComponent } from "./ve-da-dat/ve-da-dat.component";
-import { DatVeComponent } from "./dat-ve/dat-ve.component";
 
 const routes: Routes = [
   { path: "", component: TicketsComponent },
-  { path: "choose-coach", component: ChooseCoachComponent },
-  { path: "payment", component: PaymentComponent },
   { path: "ve-da-dat", component: VeDaDatComponent },
-  { path: "dat-ve", component: DatVeComponent },
 ];
 
 @NgModule({
--- src/app/pages/tickets/tickets.module.ts
+++ src/app/pages/tickets/tickets.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from "@angular/core";
 import { TicketsRoutingModule } from "./tickets-routing.module";
 
 import { TicketsComponent } from "./tickets.component";
-import { ChooseCoachComponent } from "./choose-coach/choose-coach.component";
 import { NzFormModule } from "ng-zorro-antd/form";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgZorroAntdModule } from "ng-zorro-antd";
 import { CommonModule } from "@angular/common";
-import { PaymentComponent } from "./payment/payment.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { VeDaDatComponent } from "./ve-da-dat/ve-da-dat.component";
-import { DatVeComponent } from './dat-ve/dat-ve.component';
 @NgModule({
   imports: [
     TicketsRoutingModule,
@@ -21,13 +18,7 @@ import { DatVeComponent } from './dat-ve/dat-ve.component';
     NgZorroAntdModule,
     CommonModule,
   ],
-  declarations: [
-    TicketsComponent,
-    ChooseCoachComponent,
-    PaymentComponent,
-    VeDaDatComponent,
-    DatVeComponent,
-  ],
+  declarations: [TicketsComponent, VeDaDatComponent],
   // exports: [
   //   TicketsComponent,
   //   ChooseCoachComponent,
